ff9d6cada71a4b430591d8b976e8b6fe7d509377
AboutWhite <aboutwhiterabbit@gmail.com>
2018-04-24 18:43:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/aboutwhite/speechrecognizer/MainActivity.java
@@ -0,0 +1,179 @@
+package com.example.aboutwhite.speechrecognizer;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.vikramezhil.droidspeech.DroidSpeech;
+import com.vikramezhil.droidspeech.OnDSListener;
+
+import java.util.List;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.vikramezhil.droidspeech.DroidSpeech;
+import com.vikramezhil.droidspeech.OnDSListener;
+import com.vikramezhil.droidspeech.OnDSPermissionsListener;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends Activity implements OnDSListener, OnClickListener {
+
+    public final String TAG = "Activity_DroidSpeech";
+    private DroidSpeech droidSpeech;
+    private TextView finalSpeechResult;
+    private String result = "Result: ";
+    private ImageView start;
+    private ImageView stop;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // Initialize Droid Speech
+        droidSpeech = new DroidSpeech(this, null);
+        droidSpeech.setOnDroidSpeechListener(this);
+        droidSpeech.setShowRecognitionProgressView(true);
+        droidSpeech.setOneStepResultVerify(false);
+        droidSpeech.setRecognitionProgressMsgColor(Color.BLACK);
+        droidSpeech.setOneStepVerifyConfirmTextColor(Color.BLACK);
+        droidSpeech.setOneStepVerifyRetryTextColor(Color.BLACK);
+
+        finalSpeechResult = findViewById(R.id.finalSpeechResult);
+
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+
+        start.setOnClickListener(this);
+        //stop.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        if(stop.getVisibility() == View.VISIBLE)
+        {
+            stop.performClick();
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if(stop.getVisibility() == View.VISIBLE)
+        {
+            stop.performClick();
+        }
+    }
+
+    @Override
+    public void onDroidSpeechSupportedLanguages(String currentSpeechLanguage, List<String> supportedSpeechLanguages) {
+
+    }
+
+    @Override
+    public void onDroidSpeechRmsChanged(float rmsChangedValue) {
+
+    }
+
+    @Override
+    public void onDroidSpeechLiveResult(String liveSpeechResult)
+    {
+        Log.i(TAG, "Live speech result = " + liveSpeechResult);
+    }
+
+    @Override
+    public void onDroidSpeechFinalResult(String finalSpeechResult)
+    {
+        // Setting the final speech result
+        this.finalSpeechResult.setText(finalSpeechResult);
+
+        if(droidSpeech.getContinuousSpeechRecognition())
+        {
+            int[] colorPallets1 = new int[] {Color.RED, Color.GREEN, Color.BLUE, Color.CYAN, Color.MAGENTA};
+            int[] colorPallets2 = new int[] {Color.YELLOW, Color.RED, Color.CYAN, Color.BLUE, Color.GREEN};
+
+            // Setting random color pallets to the recognition progress view
+            droidSpeech.setRecognitionProgressViewColors(new Random().nextInt(2) == 0 ? colorPallets1 : colorPallets2);
+        }
+        else
+        {
+            stop.setVisibility(View.GONE);
+            start.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onDroidSpeechClosedByUser()
+    {
+        stop.setVisibility(View.GONE);
+        start.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onDroidSpeechError(String errorMsg)
+    {
+        // Speech error
+        Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
+
+        stop.post(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                // Stop listening
+                stop.performClick();
+            }
+        });
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.start:
+
+                // Starting droid speech
+                droidSpeech.startDroidSpeechRecognition();
+
+                // Setting the view visibilities when droid speech is running
+                start.setVisibility(View.GONE);
+                stop.setVisibility(View.VISIBLE);
+
+                break;
+
+            case R.id.stop:
+
+                // Closing droid speech
+                droidSpeech.closeDroidSpeechOperations();
+
+                stop.setVisibility(View.GONE);
+                start.setVisibility(View.VISIBLE);
+
+                break;
+        }
+
+    }
+}
+
+
