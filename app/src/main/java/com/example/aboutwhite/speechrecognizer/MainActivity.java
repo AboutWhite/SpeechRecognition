e1339c42a73aa379719737ce29ebe09f4e3a49c4
AboutWhite <aboutwhiterabbit@gmail.com>
2018-05-13 18:35:15 +0200

    Sound off

--- app/src/main/java/com/example/aboutwhite/speechrecognizer/MainActivity.java
+++ app/src/main/java/com/example/aboutwhite/speechrecognizer/MainActivity.java
@@ -61,15 +61,7 @@ public class MainActivity extends Activity implements OnDSListener, OnClickListe
 
 
         AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_MUTE, 0);
-            } else {
-                audioManager.setStreamMute(AudioManager.STREAM_MUSIC, true);
-            }
-        } catch (Exception e){
-            if(audioManager == null) return;
-        }
+       audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
 
 
         // Initialize Droid Speech
